f5069a942ae2144ce26beaeffe77b57005341752
Monica Amitin <monzielvsu@gmail.com>
2019-12-02 21:10:55 -0800

    added switch and finished dowhatitsays function

--- liri.js
+++ liri.js
@@ -12,18 +12,6 @@ var liriData = process.argv[3];
 //console.log(process.argv);
 
 
-if (liriCommand === "concert-this") {
-    concertThis(liriData)
-} else if (liriCommand === "spotify-this-song") {
-    spotifyThis(liriData)
-} else if (liriCommand === "movie-this") {
-    movieThis(liriData)
-} else if (liriCommand === "do-what-it-says") {
-    doWhatItSays(liriData)
-} else {
-    console.log("error");
-}
-
 
 
 function concertThis() {
@@ -36,9 +24,11 @@ function concertThis() {
         .then(function (response) {
             //console.log(response.data);
             if (response.data[0] != undefined) {
+                console.log("-----------------------------------------------------------------------")
                 console.log("Venue Name: " + response.data[0].venue.name);
                 console.log("Venue City: " + response.data[0].venue.city);
                 console.log("Date of Event: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+                console.log("-----------------------------------------------------------------------")
             } else {
                 console.log("No events found");
             }
@@ -51,55 +41,77 @@ function concertThis() {
 function spotifyThis() {
     if (liriData === "undefined") {
         liriData = "The Sign, Ace of Base";
+        console.log(liriData);
     }
     //form API call here
     spotify.search({ type: "track", query: liriData, limit: 1 }, function (err, data) {
         if (err) {
             return console.log("Error occurred: " + err);
         }
-        //          console.log(data);
+        //console.log(data);
+        console.log("-----------------------------------------------------------------------")
         console.log("Artist: " + data.tracks.items[0].artists[0].name);
         console.log("Track: " + data.tracks.items[0].name);
         console.log("URL: " + data.tracks.items[0].preview_url);
         console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("-----------------------------------------------------------------------")
     });
 }
 
 function movieThis() {
-    if (process.argv[2] === "movie-this") {
-        console.log("movie-this works");
-        var movie = process.argv[3]
-        console.log("title: ", movie);
-        axios.get("http://www.omdbapi.com/?apikey=trilogy&s=${movie}")
-            .then(function (response) {
-                //  console.log(response.data.Search[0].imdbID);
-                var tempImdbID = response.data.Search[0].imdbID;
-                //console.log(tempImdbID);
-                axios.get("http://www.omdbapi.com/?apikey=trilogy&i=${tempImdbID}")
-                    .then(function (response2) {
-                        console.log("Title: " + response2.data.Title);
-                        console.log("Year: " + response2.data.Year);
-                        console.log("IMDB Rating: " + response2.data.imdbRating);
-                        console.log("Rotten Tomatoes Rating: " + response2.data.Ratings[1].Value)
-                        console.log("Country Produced: " + response2.data.Country);
-                        console.log("Language: " + response2.data.Language);
-                        console.log("Plot: " + response2.data.Plot);
-                        console.log("Actors: " + response2.data.Actors);
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
-
-            });
+    if (typeof liriData === "movie-this") {
+        liriData = "Mr. Nobody";
+        console.log(liriData);
     }
+    axios.get("http://www.omdbapi.com/?apikey=trilogy&s=${movie}")
+        .then(function (response) {
+            //  console.log(response.data.Search[0].imdbID);
+            var tempImdbID = response.data.Search[0].imdbID;
+            //console.log(tempImdbID);
+            axios.get("http://www.omdbapi.com/?apikey=trilogy&i=${tempImdbID}")
+                .then(function (response2) {
+                    console.log("-----------------------------------------------------------------------")
+                    console.log("Title: " + response2.data.Title);
+                    console.log("Year: " + response2.data.Year);
+                    console.log("IMDB Rating: " + response2.data.imdbRating);
+                    console.log("Rotten Tomatoes Rating: " + response2.data.Ratings[1].Value)
+                    console.log("Country Produced: " + response2.data.Country);
+                    console.log("Language: " + response2.data.Language);
+                    console.log("Plot: " + response2.data.Plot);
+                    console.log("Actors: " + response2.data.Actors);
+                    console.log("-----------------------------------------------------------------------")
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 function doWhatItSays() {
-         fs.readFile("random.txt", "utf8", function (err, data) {
-             var dataArray = data.split(",");
-             liriCommand = dataArray[0];
-             liriData = dataArray[1];
-             spotifyThis(liriData);
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        var dataArray = data.split(",");
+        liriCommand = dataArray[0];
+        liriData = dataArray[1];
+        //console.log(err);
+        spotifyThis(liriData);
+    });
+}
 
-         });
-     
+switch (liriCommand) {
+    case "concert-this":
+        concertThis();
+        break;
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+}
